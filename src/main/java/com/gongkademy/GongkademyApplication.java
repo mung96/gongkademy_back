1594df292d865771645edf0c0565b9ef3b20fdc1
mung96 <mung5079@gmail.com>
2024-12-16 23:24:00 +0900

    project init

---
+++ src/main/java/com/gongkademy/GongkademyApplication.java
@@ -0,0 +1,13 @@
+package com.gongkademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongkademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GongkademyApplication.class, args);
+    }
+
+}
