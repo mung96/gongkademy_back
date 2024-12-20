88d553dfe7b7a71f196d4bedeef759fa9f771b16
mung96 <mung5079@gmail.com>
2024-12-20 23:52:03 +0900

    test: id로 강좌 조회 기능 테스트 추가

---
+++ src/test/java/com/gongkademy/repository/CourseRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.gongkademy.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.gongkademy.domain.Course;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class CourseRepositoryTest {
+
+    @Autowired EntityManager em;
+    @Autowired CourseRepository courseRepository;
+
+    @Test
+    void id로_강좌_조회() {
+
+        //given
+        Course course = Course.builder()
+                              .title("재료역학")
+                              .thumbnail("aaa")
+                              .build();
+        em.persist(course);
+
+        //when
+        Course findCourse = courseRepository.findById(1L);
+
+        //then
+        assertEquals("재료역학",findCourse.getTitle());
+        assertEquals("aaa",findCourse.getThumbnail());
+    }
+}
