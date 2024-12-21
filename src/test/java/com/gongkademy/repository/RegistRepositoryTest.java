2f04271134c6f6b0a50c3a2c1f39a3fca2cf980a
mung96 <mung5079@gmail.com>
2024-12-21 15:09:06 +0900

    test: 강좌 수강 신청 테스트 추가

---
+++ src/test/java/com/gongkademy/repository/RegistRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.gongkademy.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.gongkademy.domain.Course;
+import com.gongkademy.domain.Member;
+import com.gongkademy.domain.Regist;
+import jakarta.persistence.EntityManager;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class RegistRepositoryTest {
+
+    @Autowired EntityManager em;
+    @Autowired RegistRepository registRepository;
+
+    @Test
+    void 강좌_수강_신청(){
+        //Given
+        Member member = Member.builder()
+                              .nickname("유저1")
+                              .email("aaaa@naver.com")
+                              .build();
+        Course course = Course.builder()
+                              .title("재료역학")
+                              .thumbnail("aaa")
+                              .build();
+        em.persist(course);
+        em.persist(member);
+
+        Regist regist = Regist.builder()
+                              .member(member)
+                              .course(course)
+                                .createdTime(LocalDateTime.now())
+                              .build();
+        //When
+        Long findRegistId = registRepository.save(regist);
+        //Then
+        assertEquals(regist.getId(),findRegistId);
+    }
+
+}
