5358642c387994b9969c687e2ce179c3185f50a0
mung96 <mung5079@gmail.com>
2024-12-21 14:39:39 +0900

    test: 강의 id로 강의 상세 조회 테스트 추가

--- src/test/java/com/gongkademy/repository/LectureRepositoryTest.java
+++ src/test/java/com/gongkademy/repository/LectureRepositoryTest.java
@@ -19,7 +19,7 @@ class LectureRepositoryTest {
     @Autowired LectureRepository lectureRepository;
 
     @Test
-    void 강좌id로_강의목록_조회(){
+    void 강좌_id로_강의목록_조회(){
         //given
         Course course = Course.builder()
                               .title("재료역학")
@@ -44,7 +44,7 @@ class LectureRepositoryTest {
         em.persist(lecture2);
 
         //when
-        List<Lecture> findLectures = lectureRepository.findLecturesByCourseId(1L);
+        List<Lecture> findLectures = lectureRepository.findLecturesByCourseId(course.getId());
 
         //then
         assertEquals(2,findLectures.size());
@@ -52,4 +52,38 @@ class LectureRepositoryTest {
         assertTrue(findLectures.contains(lecture2));
     }
 
+    @Test
+    void 강의_id로_강의상세_조회(){
+        //given
+        Course course = Course.builder()
+                              .title("재료역학")
+                              .thumbnail("aaa")
+                              .build();
+        Lecture lecture1 = Lecture.builder()
+                                  .title("재료역학이란")
+                                  .lectureOrder(1)
+                                  .url("ys5niu4Sabg&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy")
+                                  .runtime(15 * 60)
+                                  .course(course)
+                                  .build();
+        Lecture lecture2 = Lecture.builder()
+                                  .title("응력이란")
+                                  .lectureOrder(2)
+                                  .url("y1b7jfIg_2w&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy&index=2")
+                                  .runtime(16 * 60)
+                                  .course(course)
+                                  .build();
+        em.persist(course);
+        em.persist(lecture1);
+        em.persist(lecture2);
+
+        //when
+        Lecture findLecture1 = lectureRepository.findById(lecture1.getId());
+        Lecture findLecture2 = lectureRepository.findById(lecture2.getId());
+
+        //then
+        assertEquals(lecture1,findLecture1);
+        assertEquals(lecture2,findLecture2);
+
+    }
 }
