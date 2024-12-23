f7f971877bcb9140ca9b4c8ac116d757feb800c0
mung96 <mung5079@gmail.com>
2024-12-23 10:27:38 +0900

    test: 강좌 수강 철회 테스트 추가

--- src/test/java/com/gongkademy/repository/RegistRepositoryTest.java
+++ src/test/java/com/gongkademy/repository/RegistRepositoryTest.java
@@ -45,4 +45,32 @@ class RegistRepositoryTest {
         assertEquals(regist.getId(),findRegistId);
     }
 
+    @Test
+    void 회원id_강좌id_강좌_수강_철회(){
+        //given
+        //Given
+        Member member = Member.builder()
+                              .nickname("유저1")
+                              .email("aaaa@naver.com")
+                              .build();
+        Course course = Course.builder()
+                              .title("재료역학")
+                              .thumbnail("aaa")
+                              .build();
+        em.persist(course);
+        em.persist(member);
+
+        Regist regist = Regist.builder()
+                              .member(member)
+                              .course(course)
+                              .createdTime(LocalDateTime.now())
+                              .build();
+        //When
+        Long findRegistId = registRepository.save(regist);
+        Long deletedRegistId = registRepository.deleteById(member.getId(), course.getId());
+
+        //then
+        assertEquals(regist.getId(),deletedRegistId);
+    }
+
 }
