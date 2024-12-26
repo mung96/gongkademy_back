31557f1d2be8394cf766261cfc04e6816756de7c
mung96 <mung5079@gmail.com>
2024-12-26 16:50:44 +0900

    test: 게시글 id로 댓글 조회 테스트 추가

--- src/test/java/com/gongkademy/repository/CommentRepositoryTest.java
+++ src/test/java/com/gongkademy/repository/CommentRepositoryTest.java
@@ -9,6 +9,7 @@ import com.gongkademy.domain.Member;
 import com.gongkademy.domain.board.Question;
 import com.gongkademy.domain.board.Worry;
 import jakarta.persistence.EntityManager;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,6 +23,27 @@ class CommentRepositoryTest {
     EntityManager em;
     @Autowired
     CommentRepository commentRepository;
+
+    @Test
+    void 게시글_id로_댓글_조회(){
+        //given
+        Member member = Member.builder().nickname("유저1").email("aaa@naver.com").build();
+        Worry worry = Worry.builder().title("고민1").body("고민내용1").member(member).build();
+
+        em.persist(member);
+        em.persist(worry);
+
+        Comment comment1 = Comment.builder().content("댓글내용1").member(member).board(worry).build();
+        Comment comment2 = Comment.builder().content("댓글내용2").member(member).board(worry).build();
+        em.persist(comment1);
+        em.persist(comment2);
+
+        //when
+        List<Comment> findCommentList = commentRepository.findByBoardId(worry.getId());
+
+        //then
+        assertEquals(2,findCommentList.size());
+    }
 
     @Test
     void 댓글_작성(){
