29bbee5677ba320ad73ffe7742ad31c2277968e4
mung96 <mung5079@gmail.com>
2024-12-26 16:10:31 +0900

    test: 게시글 삭제 테스트 추가

--- src/test/java/com/gongkademy/repository/BoardRepositoryTest.java
+++ src/test/java/com/gongkademy/repository/BoardRepositoryTest.java
@@ -76,4 +76,28 @@ class BoardRepositoryTest {
         assertEquals(findQuestion.getLecture().getTitle(),question.getLecture().getTitle());
         assertEquals(findWorry,worry);
     }
+
+
+    @Test
+    void 게시글_삭제(){
+        //given
+        Member member = Member.builder().nickname("유저1").email("aaa@naver.com").build();
+        Course course = Course.builder().title("재료역학").thumbnail("aaa").build();
+        Lecture lecture = Lecture.builder().title("재료역학이란").lectureOrder(1).url("ys5niu4Sabg&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy")
+                                 .runtime(15 * 60).course(course).build();
+        Question question = Question.builder().title("질문1").body("질문내용1").member(member).lecture(lecture).build();
+        Worry worry = Worry.builder().title("고민1").body("고민내용1").member(member).build();
+
+        em.persist(member);
+        em.persist(course);
+        em.persist(lecture);
+        em.persist(question);
+        em.persist(worry);
+
+        //when
+        Long deleteId =  boardRepository.delete(question.getId());
+
+        //then
+        assertEquals(question.getId(),deleteId);
+    }
 }
