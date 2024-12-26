9f5515e47d7cc2a01913b67947de1f2b712ae5fc
mung96 <mung5079@gmail.com>
2024-12-26 16:39:34 +0900

    test: 게시글 id로 조회 기록 조회 테스트 추가

--- src/test/java/com/gongkademy/repository/ViewRepositoryTest.java
+++ src/test/java/com/gongkademy/repository/ViewRepositoryTest.java
@@ -9,6 +9,7 @@ import com.gongkademy.domain.View;
 import com.gongkademy.domain.board.Question;
 import com.gongkademy.domain.board.Worry;
 import jakarta.persistence.EntityManager;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -38,5 +39,26 @@ class ViewRepositoryTest {
         View findView = em.find(View.class, view.getId());
         //Then
         assertEquals(view, findView);
+    }
+
+    @Test
+    void 게시글_id로_조회기록_조회() {
+        //given
+        Member member1 = Member.builder().nickname("유저1").email("aaa@naver.com").build();
+        Member member2 = Member.builder().nickname("유저2").email("bbb@naver.com").build();
+        Worry worry = Worry.builder().title("고민1").body("고민내용1").member(member1).build();
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(worry);
+        View view1 = View.builder().board(worry).member(member1).build();
+        View view2 = View.builder().board(worry).member(member2).build();
+
+        //When
+        viewRepository.save(view1);
+        viewRepository.save(view2);
+        List<View> findViewList = viewRepository.findAllByBoardId(worry.getId());
+        //Then
+        assertEquals(2, findViewList.size());
     }
 }
