87a5cc821874468b635c4ba50a96a12c635b220b
mung96 <mung5079@gmail.com>
2024-12-26 16:00:49 +0900

    test: 회원 id로 게시글 조회 테스트 추가

--- src/test/java/com/gongkademy/repository/BoardRepositoryTest.java
+++ src/test/java/com/gongkademy/repository/BoardRepositoryTest.java
@@ -9,6 +9,7 @@ import com.gongkademy.domain.board.Board;
 import com.gongkademy.domain.board.Question;
 import com.gongkademy.domain.board.Worry;
 import jakarta.persistence.EntityManager;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,8 +30,8 @@ class BoardRepositoryTest {
         Course course = Course.builder().title("재료역학").thumbnail("aaa").build();
         Lecture lecture = Lecture.builder().title("재료역학이란").lectureOrder(1).url("ys5niu4Sabg&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy")
                                   .runtime(15 * 60).course(course).build();
-        Question question = Question.builder().title("질문1").body("질문내용1").lecture(lecture).build();
-        Worry worry = Worry.builder().title("고민1").body("고민내용1").build();
+        Question question = Question.builder().title("질문1").body("질문내용1").member(member).lecture(lecture).build();
+        Worry worry = Worry.builder().title("고민1").body("고민내용1").member(member).build();
 
         em.persist(member);
         em.persist(course);
@@ -41,9 +42,11 @@ class BoardRepositoryTest {
         //when
         Board findQuestion = boardRepository.findById(question.getId());
         Board findWorry = boardRepository.findById(worry.getId());
+        List<Board> findBoardList = boardRepository.findAllByMemberId(member.getId());
 
         //then
-        assertEquals(question,findQuestion);
-        assertEquals(worry,findWorry);
+        assertEquals(findQuestion,question);
+        assertEquals(findWorry,worry);
+        assertEquals(2,findBoardList.size());
     }
 }
