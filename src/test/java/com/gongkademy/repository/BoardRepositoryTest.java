15125f7bfbabb11461c9f1e52525c4dd56659850
mung96 <mung5079@gmail.com>
2024-12-26 16:07:26 +0900

    test: 게시글 저장 테스트 추가

--- src/test/java/com/gongkademy/repository/BoardRepositoryTest.java
+++ src/test/java/com/gongkademy/repository/BoardRepositoryTest.java
@@ -49,4 +49,31 @@ class BoardRepositoryTest {
         assertEquals(findWorry,worry);
         assertEquals(2,findBoardList.size());
     }
+
+    @Test
+    void 게시글_작성(){
+        //given
+        Member member = Member.builder().nickname("유저1").email("aaa@naver.com").build();
+        Course course = Course.builder().title("재료역학").thumbnail("aaa").build();
+        Lecture lecture = Lecture.builder().title("재료역학이란").lectureOrder(1).url("ys5niu4Sabg&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy")
+                                 .runtime(15 * 60).course(course).build();
+        Question question = Question.builder().title("질문1").body("질문내용1").member(member).lecture(lecture).build();
+        Worry worry = Worry.builder().title("고민1").body("고민내용1").member(member).build();
+
+        em.persist(member);
+        em.persist(course);
+        em.persist(lecture);
+
+        //when
+        boardRepository.save(question);
+        boardRepository.save(worry);
+
+        Question findQuestion = em.find(Question.class,question.getId());
+        Worry findWorry = em.find(Worry.class,worry.getId());
+
+        //then
+        assertEquals(findQuestion,question);
+        assertEquals(findQuestion.getLecture().getTitle(),question.getLecture().getTitle());
+        assertEquals(findWorry,worry);
+    }
 }
