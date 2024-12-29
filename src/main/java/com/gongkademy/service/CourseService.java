8bf5738690fadc31639b5d26f3c88cd7b6045b8c
송현명 <mung5079@gmail.com>
2024-12-30 01:09:12 +0900

    Feat: 강좌상세 service 구현 (#22)
    * feat: 강좌 상세 조회 service interface 구현
    
    * feat: 강좌 상세 조회 service 기능 구현
    
    * test: 강좌 상세 조회 테스트 추가
    
    * feat: 강좌 상세 비로그인 유저 기능 구현
    
    * feat: 강의 목차 조회 service 기능 구현
    
    * test: 강의 목차 조회 테스트 추가

--- src/main/java/com/gongkademy/service/CourseService.java
+++ src/main/java/com/gongkademy/service/CourseService.java
@@ -1,8 +1,16 @@
 package com.gongkademy.service;
 
+import com.gongkademy.service.dto.CourseDetailResponse;
+import com.gongkademy.service.dto.LectureListResponse;
+import java.util.List;
+
 public interface CourseService {
 
     Long registerCourse(Long memberId, Long courseId);
 
     Long dropCourse(Long memberId, Long courseId);
+
+    CourseDetailResponse findCourseDetail(Long memberId, Long courseId);
+
+    LectureListResponse findLectureList(Long memberId, Long courseId);
 }
--- src/main/java/com/gongkademy/service/CourseServiceImpl.java
+++ src/main/java/com/gongkademy/service/CourseServiceImpl.java
@@ -5,13 +5,22 @@ import static com.gongkademy.exception.ErrorCode.COURSE_NOT_FOUND;
 import static com.gongkademy.exception.ErrorCode.MEMBER_NOT_FOUND;
 
 import com.gongkademy.domain.Course;
+import com.gongkademy.domain.Lecture;
 import com.gongkademy.domain.Member;
+import com.gongkademy.domain.Play;
 import com.gongkademy.domain.Register;
 import com.gongkademy.exception.CustomException;
 import com.gongkademy.exception.ErrorCode;
 import com.gongkademy.repository.CourseRepository;
+import com.gongkademy.repository.LectureRepository;
 import com.gongkademy.repository.MemberRepository;
+import com.gongkademy.repository.PlayRepository;
 import com.gongkademy.repository.RegisterRepository;
+import com.gongkademy.service.dto.CourseDetailResponse;
+import com.gongkademy.service.dto.LectureItemDto;
+import com.gongkademy.service.dto.LectureListResponse;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,9 +33,11 @@ public class CourseServiceImpl implements CourseService {
     private final MemberRepository memberRepository;
     private final CourseRepository courseRepository;
     private final RegisterRepository registerRepository;
+    private final LectureRepository lectureRepository;
+    private final PlayRepository playRepository;
 
-    @Override
     //수강 신청
+    @Override
     public Long registerCourse(Long memberId, Long courseId) {
         //가입한 회원인지 확인
         Member member = memberRepository.findById(memberId)
@@ -46,8 +57,8 @@ public class CourseServiceImpl implements CourseService {
         return register.getId();
     }
 
-    @Override
     //수강 취소
+    @Override
     public Long dropCourse(Long memberId, Long courseId) {
         Register register = registerRepository.findByMemberIdAndCourseId(memberId, courseId)
                                               .orElseThrow(() -> new CustomException(ErrorCode.REGISTER_NOT_FOUND));
@@ -55,4 +66,66 @@ public class CourseServiceImpl implements CourseService {
         return register.getId();
     }
 
+    //강좌 상세 조회
+    @Override
+    @Transactional(readOnly = true)
+    public CourseDetailResponse findCourseDetail(Long memberId, Long courseId) {
+        Course course = courseRepository.findById(courseId)
+                                        .orElseThrow(()-> new CustomException(COURSE_NOT_FOUND));
+        List<Lecture> lectureList = lectureRepository.findLecturesByCourseId(courseId);
+
+        //수강 중인지 확인\
+        boolean isRegister = false;
+        if(memberId != null){
+            isRegister = registerRepository.findByMemberIdAndCourseId(memberId, courseId).isPresent();
+        }
+
+        //총 강좌 시간
+        int courseTime = 0;
+        for(Lecture lecture:lectureList){
+            courseTime += lecture.getRuntime();
+        }
+
+        return CourseDetailResponse.builder()
+                                    .title(course.getTitle())
+                                    .thumbnail(course.getThumbnail())
+                                    .courseNote(course.getCourseNote())
+                                    .courseTime(courseTime)
+                                    .isRegister(isRegister)
+                                    .build();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public LectureListResponse findLectureList(Long memberId, Long courseId) {
+        List<Lecture> lectureList = lectureRepository.findLecturesByCourseId(courseId);
+
+        //수강 중인지 확인
+        boolean isRegister = false;
+        if(memberId != null){
+            isRegister = registerRepository.findByMemberIdAndCourseId(memberId, courseId).isPresent();
+        }
+
+        //LectureListResponse 생성
+        List<LectureItemDto> lectureItemDtoList = new ArrayList<>();
+        for(Lecture lecture:lectureList){
+            boolean isComplete = false;
+            if(memberId != null){
+                isComplete = playRepository.findByMemberIdAndLectureId(memberId, lecture.getId())
+                                           .map(play -> play.getLastPlayedTime() > play.getLecture().getRuntime() - 10)
+                                           .orElse(false);
+            }
+
+           lectureItemDtoList.add(LectureItemDto.builder()
+                                                .lectureId(lecture.getId())
+                                                .title(lecture.getTitle())
+                                                .runtime(lecture.getRuntime())
+                                                .isComplete(isComplete)
+                                                .build());
+        }
+        return LectureListResponse.builder()
+                                  .lectureList(lectureItemDtoList)
+                                  .isRegister(isRegister)
+                                  .build();
+    }
 }
