8e8f65d2bde18b7990b622b300b265c8d944ef1e
mung96 <mung5079@gmail.com>
2024-12-30 01:07:18 +0900

    test: 강의 목차 조회 테스트 추가

--- src/test/java/com/gongkademy/service/CourseServiceTest.java
+++ src/test/java/com/gongkademy/service/CourseServiceTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.gongkademy.domain.Course;
 import com.gongkademy.domain.Lecture;
 import com.gongkademy.domain.Member;
+import com.gongkademy.domain.Play;
 import com.gongkademy.domain.Register;
 import com.gongkademy.exception.CustomException;
 import com.gongkademy.exception.ErrorCode;
 import com.gongkademy.repository.RegisterRepository;
 import com.gongkademy.service.dto.CourseDetailResponse;
+import com.gongkademy.service.dto.LectureListResponse;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -95,6 +97,7 @@ class CourseServiceTest {
         em.persist(course);
         em.persist(lecture1);
         em.persist(lecture2);
+
         //When
         courseService.registerCourse(member1.getId(),course.getId());
         CourseDetailResponse registerCourseDetail = courseService.findCourseDetail(member1.getId(),course.getId());
@@ -105,23 +108,70 @@ class CourseServiceTest {
         //수강한 유저
         assertEquals(course.getTitle(), registerCourseDetail.getTitle());
         assertEquals(course.getThumbnail(), registerCourseDetail.getThumbnail());
-        assertEquals(null, registerCourseDetail.getCourseNote());
+        assertNull(registerCourseDetail.getCourseNote());
         assertTrue(registerCourseDetail.isRegister());
         assertEquals(lecture1.getRuntime()+lecture2.getRuntime(), registerCourseDetail.getCourseTime());
 
         //수강하지 않은 유저
         assertEquals(course.getTitle(), unregisterCourseDetail.getTitle());
         assertEquals(course.getThumbnail(), unregisterCourseDetail.getThumbnail());
-        assertEquals(null, unregisterCourseDetail.getCourseNote());
+        assertNull(unregisterCourseDetail.getCourseNote());
         assertFalse(unregisterCourseDetail.isRegister());
         assertEquals(lecture1.getRuntime()+lecture2.getRuntime(), unregisterCourseDetail.getCourseTime());
 
         //로그인 안한 유저
         assertEquals(course.getTitle(), nonLoginCourseDetail.getTitle());
         assertEquals(course.getThumbnail(), nonLoginCourseDetail.getThumbnail());
-        assertEquals(null, nonLoginCourseDetail.getCourseNote());
+        assertNull(nonLoginCourseDetail.getCourseNote());
         assertFalse(nonLoginCourseDetail.isRegister());
         assertEquals(lecture1.getRuntime()+lecture2.getRuntime(), nonLoginCourseDetail.getCourseTime());
     }
 
+    @Test
+    void 강좌의_강의_목록_조회(){
+        //Given
+        Member member1 = Member.builder().nickname("유저1").email("aaa@naver.com").build();
+        Member member2 = Member.builder().nickname("유저2").email("bbb@naver.com").build();
+        Course course = Course.builder().title("재료역학").thumbnail("aaa").build();
+        Lecture lecture1 = Lecture.builder().title("재료역학이란").lectureOrder(1).url("ys5niu4Sabg&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy").runtime(15 * 60)
+                                  .course(course).build();
+        Lecture lecture2 = Lecture.builder().title("응력이란").lectureOrder(2).url("y1b7jfIg_2w&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy&index=2").runtime(16 * 60)
+                                  .course(course).build();
+        Play play1 = Play.builder().lastPlayedTime(lecture1.getRuntime()-30).member(member1).lecture(lecture1).build();
+        Play play2 = Play.builder().lastPlayedTime(lecture2.getRuntime()-9).member(member1).lecture(lecture2).build();
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(course);
+        em.persist(lecture1);
+        em.persist(lecture2);
+        em.persist(play1);
+        em.persist(play2);
+
+        //When
+        courseService.registerCourse(member1.getId(),course.getId());
+        LectureListResponse registerLectureList = courseService.findLectureList(member1.getId(),course.getId());
+        LectureListResponse unRegisterLectureList = courseService.findLectureList(member2.getId(),course.getId());
+        LectureListResponse nonLoginLectureList = courseService.findLectureList(null,course.getId());
+
+        //Then
+        //수강한 유저
+        assertEquals(2, registerLectureList.getLectureList().size());
+        assertTrue(registerLectureList.isRegister());
+        assertFalse(registerLectureList.getLectureList().get(0).isComplete());
+        assertTrue(registerLectureList.getLectureList().get(1).isComplete());
+
+
+        //수강하지 않은 유저
+        assertEquals(2, unRegisterLectureList.getLectureList().size());
+        assertFalse(unRegisterLectureList.isRegister());
+        assertFalse(unRegisterLectureList.getLectureList().get(0).isComplete());
+        assertFalse(unRegisterLectureList.getLectureList().get(1).isComplete());
+
+        //로그인 안한 유저
+        assertEquals(2, nonLoginLectureList.getLectureList().size());
+        assertFalse(nonLoginLectureList.isRegister());
+        assertFalse(nonLoginLectureList.getLectureList().get(0).isComplete());
+        assertFalse(nonLoginLectureList.getLectureList().get(1).isComplete());
+    }
 }
