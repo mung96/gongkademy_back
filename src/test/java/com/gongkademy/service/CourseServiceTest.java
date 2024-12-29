d0a6174207bcc34515497469edf2bb3f45b75a4e
mung96 <mung5079@gmail.com>
2024-12-29 22:22:13 +0900

    test: 수강 취소 테스트 추가

--- src/test/java/com/gongkademy/service/CourseServiceTest.java
+++ src/test/java/com/gongkademy/service/CourseServiceTest.java
@@ -1,6 +1,7 @@
 package com.gongkademy.service;
 
 import static com.gongkademy.exception.ErrorCode.REGISTERED_COURSE;
+import static com.gongkademy.exception.ErrorCode.REGISTER_NOT_FOUND;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.gongkademy.domain.Course;
@@ -41,14 +42,41 @@ class CourseServiceTest {
 
         //When
         Long registerCourseId = courseService.registerCourse(member.getId(), course.getId());
+        Register findRegister = em.find(Register.class, registerCourseId);
 
         CustomException e = assertThrows(CustomException.class, () -> {
             courseService.registerCourse(member.getId(), course.getId());
         });
 
         //Then
-        assertEquals(1, registerCourseId);
+        assertEquals(findRegister.getId(), registerCourseId);
         assertEquals(REGISTERED_COURSE,e.getErrorCode());
+    }
+
+    @Test
+    void 수강_취소() {
+        //Given
+        Member member1 = Member.builder().nickname("유저1").email("aaa@naver.com").build();
+        Member member2 = Member.builder().nickname("유저2").email("bbb@naver.com").build();
+        Course course = Course.builder()
+                              .title("재료역학")
+                              .thumbnail("aaa")
+                              .build();
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(course);
+
+        //when
+        Long registerCourseId1 = courseService.registerCourse(member1.getId(), course.getId());
+
+        Long deletedCourseId = courseService.dropCourse(member1.getId(), course.getId());
+        CustomException e = assertThrows(CustomException.class, () -> {
+            courseService.dropCourse(member2.getId(), course.getId());
+        });
+
+        //Then
+        assertEquals(registerCourseId1, deletedCourseId);
+        assertEquals(REGISTER_NOT_FOUND, e.getErrorCode());
     }
 
 }
