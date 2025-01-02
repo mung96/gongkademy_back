354558b9de15e1ddb26d0fe8f2c37cdf2b299929
mung96 <mung5079@gmail.com>
2025-01-02 17:28:55 +0900

    feat: 게시글 작성 기능 구현

--- src/main/java/com/gongkademy/service/BoardService.java
+++ src/main/java/com/gongkademy/service/BoardService.java
@@ -1,11 +1,12 @@
 package com.gongkademy.service;
 
 import com.gongkademy.domain.board.Board;
+import com.gongkademy.domain.board.BoardCategory;
 import com.gongkademy.service.dto.WriteBoardRequest;
 
 public interface BoardService {
 
-    Long write(Long memberId, WriteBoardRequest board);
+    Long write(Long memberId, WriteBoardRequest board, BoardCategory category);
     Long edit(Long memberId, WriteBoardRequest board);
     Long delete(Long memberId, Long boardId);
 }
--- src/main/java/com/gongkademy/service/BoardServiceImpl.java
+++ src/main/java/com/gongkademy/service/BoardServiceImpl.java
@@ -1,12 +1,58 @@
 package com.gongkademy.service;
 
-import com.gongkademy.service.dto.WriteBoardRequest;
+import static com.gongkademy.exception.ErrorCode.LECTURE_NOT_FOUND;
+import static com.gongkademy.exception.ErrorCode.MEMBER_NOT_FOUND;
 
+import com.gongkademy.domain.Lecture;
+import com.gongkademy.domain.Member;
+import com.gongkademy.domain.board.Board;
+import com.gongkademy.domain.board.BoardCategory;
+import com.gongkademy.domain.board.Question;
+import com.gongkademy.domain.board.Worry;
+import com.gongkademy.exception.CustomException;
+import com.gongkademy.exception.ErrorCode;
+import com.gongkademy.repository.BoardRepository;
+import com.gongkademy.repository.LectureRepository;
+import com.gongkademy.repository.MemberRepository;
+import com.gongkademy.service.dto.WriteBoardRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class BoardServiceImpl implements BoardService{
 
+    private final MemberRepository memberRepository;
+    private final BoardRepository boardRepository;
+    private final LectureRepository lectureRepository;
+
     @Override
-    public Long write(Long memberId, WriteBoardRequest board) {
-        return 0L;
+    public Long write(Long memberId, WriteBoardRequest board, BoardCategory category) {
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(MEMBER_NOT_FOUND));
+        Board newBoard = null;
+        if (category == BoardCategory.WORRY) {
+            newBoard = Worry.builder()
+                                     .title(board.getTitle())
+                                     .body(board.getBody())
+                                     .member(member)
+                                     .build();
+
+        } else if (category == BoardCategory.QUESTION) {
+            Lecture lecture = lectureRepository.findById(board.getLectureId()).orElseThrow(() -> new CustomException(LECTURE_NOT_FOUND));
+            newBoard = Question.builder()
+                                        .title(board.getTitle())
+                                        .body(board.getBody())
+                                        .member(member)
+                                        .lecture(lecture)
+                                        .build();
+        }
+
+        if(newBoard == null) throw new CustomException(ErrorCode.BOARD_CATEGORY_NOT_FOUND);
+        boardRepository.save(newBoard);
+
+        return newBoard.getId();
     }
 
     @Override
