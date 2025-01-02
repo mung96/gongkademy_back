19d84014befe8c75adc2954561a82899a0ed7b68
mung96 <mung5079@gmail.com>
2025-01-02 19:52:35 +0900

    test: 게시글 service 삭제 테스트 추가

--- src/test/java/com/gongkademy/service/BoardServiceTest.java
+++ src/test/java/com/gongkademy/service/BoardServiceTest.java
@@ -2,6 +2,8 @@ package com.gongkademy.service;
 
 import static com.gongkademy.domain.board.BoardCategory.QUESTION;
 import static com.gongkademy.domain.board.BoardCategory.WORRY;
+import static com.gongkademy.exception.ErrorCode.NOT_WRITER;
+import static com.gongkademy.exception.ErrorCode.REGISTERED_COURSE;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.gongkademy.domain.Course;
@@ -10,6 +12,7 @@ import com.gongkademy.domain.Member;
 import com.gongkademy.domain.board.Board;
 import com.gongkademy.domain.board.Question;
 import com.gongkademy.domain.board.Worry;
+import com.gongkademy.exception.CustomException;
 import com.gongkademy.service.dto.WriteBoardRequest;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
@@ -50,4 +53,37 @@ class BoardServiceTest {
         assertEquals(questionRequest.getLectureId(),findQuestion.getLecture().getId());
         assertEquals(worryRequest.getTitle(),findWorry.getTitle());
     }
+
+    @Test
+    void 게시글_삭제(){
+            //given
+            Member member1 = Member.builder().nickname("유저1").email("aaa@naver.com").build();
+            Member member2 = Member.builder().nickname("유저2").email("bbb@naver.com").build();
+            Course course = Course.builder().title("재료역학").thumbnail("aaa").build();
+            Lecture lecture = Lecture.builder().title("재료역학이란").lectureOrder(1).url("ys5niu4Sabg&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy")
+                                     .runtime(15 * 60).course(course).build();
+
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(course);
+            em.persist(lecture);
+
+            WriteBoardRequest worryRequest = WriteBoardRequest.builder().title("고민1").body("고민내용1").build();
+            WriteBoardRequest questionRequest = WriteBoardRequest.builder().title("질문1").body("질문내용1").lectureId(lecture.getId()).build();
+            //when
+            Long worryId = boardService.write(member1.getId(),worryRequest ,WORRY);
+            Long questionId = boardService.write(member1.getId(),questionRequest , QUESTION);
+
+            boardService.delete(member1.getId(),worryId);
+            CustomException e = assertThrows(CustomException.class, () -> {
+                boardService.delete(member2.getId(), questionId);
+            });
+
+            Worry findWorry = em.find(Worry.class,worryId);
+//
+            //then
+            assertNull(findWorry);
+            assertEquals(NOT_WRITER,e.getErrorCode());
+
+        }
 }
