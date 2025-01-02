a3bec6a55817224675b682e5929a0a796fb446d8
mung96 <mung5079@gmail.com>
2025-01-02 20:11:42 +0900

    test: 게시글 수정 테스트 추가

--- src/test/java/com/gongkademy/service/BoardServiceTest.java
+++ src/test/java/com/gongkademy/service/BoardServiceTest.java
@@ -13,6 +13,7 @@ import com.gongkademy.domain.board.Board;
 import com.gongkademy.domain.board.Question;
 import com.gongkademy.domain.board.Worry;
 import com.gongkademy.exception.CustomException;
+import com.gongkademy.service.dto.EditBoardRequest;
 import com.gongkademy.service.dto.WriteBoardRequest;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
@@ -53,6 +54,33 @@ class BoardServiceTest {
         assertEquals(questionRequest.getLectureId(),findQuestion.getLecture().getId());
         assertEquals(worryRequest.getTitle(),findWorry.getTitle());
     }
+    @Test
+    void 게시글_수정(){
+        //given
+        Member member = Member.builder().nickname("유저1").email("aaa@naver.com").build();
+        Course course = Course.builder().title("재료역학").thumbnail("aaa").build();
+        Lecture lecture1 = Lecture.builder().title("재료역학이란").lectureOrder(1).url("ys5niu4Sabg&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy")
+                                 .runtime(15 * 60).course(course).build();
+        Lecture lecture2 = Lecture.builder().title("재료역학이란").lectureOrder(2).url("ys5niu4Sabg&list=PLwzYFiJ0Ed6kGyX0M_IW1LGiLO_Ggh4Jy")
+                                 .runtime(15 * 60).course(course).build();
+
+        em.persist(member);
+        em.persist(course);
+        em.persist(lecture1);
+        em.persist(lecture2);
+
+        WriteBoardRequest questionRequest = WriteBoardRequest.builder().title("질문1").body("질문내용1").lectureId(lecture1.getId()).build();
+        //when
+        Long questionId = boardService.write(member.getId(),questionRequest , QUESTION);
+
+        boardService.edit(member.getId(), questionId, EditBoardRequest.builder().body("고민내용2").lectureId(lecture2.getId()).build(), QUESTION);
+        Question findQuestion = em.find(Question.class,questionId);
+
+        //then
+        assertEquals("질문1",findQuestion.getTitle());
+        assertEquals("고민내용2",findQuestion.getBody());
+        assertEquals(lecture2.getId(),findQuestion.getLecture().getId());
+    }
 
     @Test
     void 게시글_삭제(){
@@ -80,10 +108,9 @@ class BoardServiceTest {
             });
 
             Worry findWorry = em.find(Worry.class,worryId);
-//
+
             //then
             assertNull(findWorry);
             assertEquals(NOT_WRITER,e.getErrorCode());
-
         }
 }
