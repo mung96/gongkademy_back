69388b2975cd225f836b9cd2c70b1468394638ab
송현명 <mung5079@gmail.com>
2025-01-05 22:44:40 +0900

    feat: member 회원가입 repository 구현 (#30)

--- src/main/java/com/gongkademy/repository/MemberRepository.java
+++ src/main/java/com/gongkademy/repository/MemberRepository.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 public interface MemberRepository {
     Optional<Member> findById(Long memberId);
     Optional<Member> findByNickname(String nickname);
+    Long save(Member member);
 }
