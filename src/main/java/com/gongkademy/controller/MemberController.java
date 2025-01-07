5c41df14064e779dc45df0bed89c2fb6b26c669c
mung96 <mung5079@gmail.com>
2025-01-07 23:49:48 +0900

    feat: updateProfile Valid 설정

--- src/main/java/com/gongkademy/controller/MemberController.java
+++ src/main/java/com/gongkademy/controller/MemberController.java
@@ -1,10 +1,14 @@
 package com.gongkademy.controller;
 
 import com.gongkademy.service.MemberService;
+import com.gongkademy.service.dto.PrincipalDetails;
+import com.gongkademy.service.dto.UpdateProfileRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +19,9 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping
-    public void updateProfile(@AuthenticationPrincipal OAuth2User oAuth2User) {
-//        @RequestBody UpdateProfileRequest updateProfileRequest
-        System.out.println("test");
-        System.out.println(oAuth2User.getAttributes());
+    @PatchMapping
+    public void updateProfile(@AuthenticationPrincipal PrincipalDetails principalDetails
+            ,@Valid @RequestBody UpdateProfileRequest updateProfileRequest) {
+        memberService.updateProfile(principalDetails.getMember().getId(), updateProfileRequest);
     }
 }
