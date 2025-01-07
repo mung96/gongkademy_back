bf6f64c65e0c461628ab19cf13867a031e0b563e
송현명 <mung5079@gmail.com>
2025-01-08 00:19:12 +0900

    Feat: member controller 구현 (#33)
    * feat: updateProfile Valid 설정
    
    * feat: GlobalExceptionHandler 등록

--- src/main/java/com/gongkademy/exception/ErrorCode.java
+++ src/main/java/com/gongkademy/exception/ErrorCode.java
@@ -1,9 +1,11 @@
 package com.gongkademy.exception;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
+@Getter
 public enum ErrorCode {
 
 
--- src/main/java/com/gongkademy/service/dto/UpdateProfileRequest.java
+++ src/main/java/com/gongkademy/service/dto/UpdateProfileRequest.java
@@ -1,12 +1,19 @@
 package com.gongkademy.service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class UpdateProfileRequest {
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Length(min=2, max=20,message = "닉네임은 2자 이상 20자 이하입니다.")
+    @Pattern(regexp="^[a-zA-Z가-힣]*$", message="닉네임은 오직 문자만 포함할 수 있습니다.")
     private String nickname;
 }
