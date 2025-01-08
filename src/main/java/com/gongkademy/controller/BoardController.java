0bb0fc844c16a560eb07aaf895e216df8b518bfc
mung96 <mung5079@gmail.com>
2025-01-08 19:05:16 +0900

    feat: 게시글 목록 조회 API 구현

--- src/main/java/com/gongkademy/controller/BoardController.java
+++ src/main/java/com/gongkademy/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.gongkademy.controller;
 
+import com.gongkademy.domain.board.BoardCategory;
 import com.gongkademy.service.BoardService;
+import com.gongkademy.service.dto.BoardListResponse;
 import com.gongkademy.service.dto.LectureDetailResponse;
 import com.gongkademy.service.dto.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
@@ -8,8 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -20,6 +24,11 @@ public class BoardController {
     private final BoardService boardService;
 
     //게시글 목록 조회
+    @GetMapping
+    public ResponseEntity<BoardListResponse> getBoards(@RequestParam BoardCategory category){
+        BoardListResponse boardList = boardService.findBoardList(category, 0);
+        return ResponseEntity.ok(boardList);
+    }
 
     //게시글 상세 조회
 
@@ -28,7 +37,7 @@ public class BoardController {
     //게시글 수정
 
     //게시글 삭제
-    @DeleteMapping("/boards/{boardId}")
+    @DeleteMapping("/{boardId}")
     public ResponseEntity<?> deleteBoard(@AuthenticationPrincipal PrincipalDetails principalDetails
             , @PathVariable Long boardId) {
         boardService.delete(principalDetails.getMember().getId(),boardId);
