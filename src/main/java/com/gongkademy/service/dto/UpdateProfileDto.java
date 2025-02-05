f4c7c26292f0e12d98056fecc65c976fe92cb208
mung96 <mung5079@gmail.com>
2025-02-06 03:38:46 +0900

    fix: 프로필 수정 API 수정

--- src/main/java/com/gongkademy/service/dto/UpdateProfileDto.java
+++ src/main/java/com/gongkademy/service/dto/UpdateProfileDto.java
@@ -2,18 +2,20 @@ package com.gongkademy.service.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class UpdateProfileRequest {
+@Builder
+public class UpdateProfileDto {
 
     @NotBlank(message = "닉네임을 입력해주세요.")
-    @Length(min=2, max=20,message = "닉네임은 2자 이상 20자 이하입니다.")
+    @Size(min=2, max=20, message = "닉네임은 2자 이상 20자 이하입니다.")
     @Pattern(regexp="^[a-zA-Z가-힣]*$", message="닉네임은 오직 문자만 포함할 수 있습니다.")
     private String nickname;
 }
