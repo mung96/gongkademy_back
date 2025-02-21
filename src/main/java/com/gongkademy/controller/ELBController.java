96211b29305e38e4e213baef26f7985496dedb3b
mung96 <mung5079@gmail.com>
2025-02-21 10:50:57 +0900

    feat: health check api 로그 추가

--- src/main/java/com/gongkademy/controller/ELBController.java
+++ src/main/java/com/gongkademy/controller/ELBController.java
@@ -11,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@Log4j2
 public class ELBController {
     @GetMapping("/health")
     public HttpStatus healthCheck(){
+
+        log.info("ELB Health Check 요청이 들어왔습니다.");
+
         return HttpStatus.OK;
     }
 }
