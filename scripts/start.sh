7debd145609531353a2936ea3aa890b01664602f
mung96 <mung5079@gmail.com>
2025-03-06 22:35:43 +0900

    fix: idle_port로직 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,14 +9,9 @@ PROJECT_NAME=gongkademy
 JAR_NAME=project.jar
 
 # 현재 실행 중인 포트 확인
-CURRENT_PROFILE=$(find_idle_profile)
-if [ "$CURRENT_PROFILE" == "prod1" ]; then
-  IDLE_PORT=8082
-  IDLE_PROFILE=prod2
-else
-  IDLE_PORT=8081
-  IDLE_PROFILE=prod1
-fi
+IDLE_PORT=$(find_idle_profile)
+IDLE_PROFILE=$(find_idle_port)
+
 
 echo "> 배포할 프로파일: $IDLE_PROFILE (포트: $IDLE_PORT)"
 
