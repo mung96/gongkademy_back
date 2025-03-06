b55aad6c35212b1e469b76e1a23ad213dfab835d
mung96 <mung5079@gmail.com>
2025-03-06 20:37:10 +0900

    chore: 필요없는 sh파일 삭제

--- scripts/start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/gongkademy_back
-sudo fuser -k -n tcp 80 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
-echo "--------------- 서버 배포 끝 -----------------"
