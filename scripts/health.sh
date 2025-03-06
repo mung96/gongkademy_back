504742ba0cddb998c7e726e068a27e33b85be28b
mung96 <mung5079@gmail.com>
2025-03-06 21:37:12 +0900

    fix: start이후 nginx 트래픽 변경 로직 수정

--- scripts/health.sh
+++ scripts/health.sh
@@ -3,7 +3,6 @@
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
-source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
 
@@ -20,7 +19,7 @@ do
   if [ ${UP_COUNT} -ge 1 ]
   then
       echo "> Health check 성공"
-      switch_proxy
+
       break
   else
       echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
