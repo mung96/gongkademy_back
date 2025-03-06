a94fb290986ae009ddcb965c3acb93163c2bfb4e
mung96 <mung5079@gmail.com>
2025-03-06 20:45:51 +0900

    fix:L profile.sh dev에서 prod로 변경

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -9,16 +9,16 @@ function find_idle_profile()
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
-        CURRENT_PROFILE=dev2
+        CURRENT_PROFILE=prod2
     else
         CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == dev1 ]
     then
-      IDLE_PROFILE=dev2 # 2
+      IDLE_PROFILE=prod2 # 2
     else
-      IDLE_PROFILE=dev1
+      IDLE_PROFILE=prod1
     fi
 
     echo "${IDLE_PROFILE}" # 3
@@ -29,7 +29,7 @@ function find_idle_port()
 {
     IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDLE_PROFILE} == dev1 ]
+    if [ ${IDLE_PROFILE} == prod1 ]
     then
       echo "8081"
     else
