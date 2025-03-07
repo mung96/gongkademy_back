0dfc244bc7cd61a610639962fc71193611001a45
mung96 <mung5079@gmail.com>
2025-03-07 09:32:42 +0900

    fix: port 변경 변수 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,28 +9,28 @@ PROJECT_NAME=gongkademy
 JAR_NAME=project.jar
 
 # 현재 실행 중인 포트 확인
-IDLE_PORT=$(find_idle_port)
-IDLE_PROFILE=$(find_idle_profile)
+NEW_PORT=$(find_idle_port)
+NEW_PROFILE=$(find_idle_profile)
 
 
-echo "> 배포할 프로파일: $IDLE_PROFILE (포트: $IDLE_PORT)"
+echo "> 배포할 프로파일: $NEW_PROFILE (포트: $NEW_PORT)"
 
 # JAR 파일 실행
 echo "> JAR 실행 권한 추가"
 chmod +x $REPOSITORY/current/$JAR_NAME
 
-echo "> 새 애플리케이션 실행 (포트: $IDLE_PORT)"
+echo "> 새 애플리케이션 실행 (포트: $NEW_PORT)"
 nohup java -jar \
-    -Dserver.port=$IDLE_PORT \
-    -Dspring.profiles.active=$IDLE_PROFILE \
-    $REPOSITORY/current/$JAR_NAME > $REPOSITORY/current/output-$IDLE_PROFILE.log 2>&1 &
+    -Dserver.port=$NEW_PORT \
+    -Dspring.profiles.active=$NEW_PROFILE \
+    $REPOSITORY/current/$JAR_NAME > $REPOSITORY/current/output-$NEW_PROFILE.log 2>&1 &
 
-echo "> 새 애플리케이션이 실행되었습니다. (포트: $IDLE_PORT)"
+echo "> 새 애플리케이션이 실행되었습니다. (포트: $NEW_PORT)"
 
 # Nginx 포트 변경
 echo "> Nginx 설정 변경 및 재시작"
 sleep 10  # 새로운 서비스가 완전히 실행될 시간을 줌
-echo "set \$service_url http://127.0.0.1:$IDLE_PORT;" | sudo tee /etc/nginx/conf.d/service-url.inc
+echo "set \$service_url http://127.0.0.1:$NEW_PORT;" | sudo tee /etc/nginx/conf.d/service-url.inc
 sudo systemctl reload nginx
 
-echo "> 배포 완료! 현재 서비스 포트: $IDLE_PORT"
+echo "> 배포 완료! 현재 서비스 포트: $NEW_PORT"
