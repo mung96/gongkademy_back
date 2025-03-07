b97cef9428198d1d154d186989d1347aef3f2658
mung96 <mung5079@gmail.com>
2025-03-07 14:32:04 +0900

    refactor: main deploy 스크립트 변경

--- scripts/health.sh
+++ scripts/health.sh
@@ -4,11 +4,8 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-ACTIVE_PORT=$(find_active_port)
-
 echo "> Health Check Start!"
-echo "> ACTIVE_PORT: $ACTIVE_PORT"
-echo "> curl -s http://localhost:$ACTIVE_PORT/profile"
+
 sleep 10
 
 for RETRY_COUNT in {1..10}
